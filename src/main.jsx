ac9e5fb5a69d323667603e5ab1da419387b19e8b
Programacion7 <Programacion7@cfl414.com>
2024-09-09 11:46:14 -0300

    modificamos el el archivo main, agregamos a la carpeta componentes los archivos Home,Info y Proximamente

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,45 @@
+//Modulos 
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+//Componentes
 import App from './App.jsx'
 import Header from './componentes/Header.jsx'
 import Footer from './componentes/Footer.jsx'
+import Home from './componentes/Home.jsx'
+import Info from './componentes/info.jsx'
+import Proximamente from './componentes/Proximamente.jsx'
 
-createRoot(document.getElementById('root')).render(
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: (<div>Error</div>)
+    
+  },
+  {
+    path: "/discos",
+    element: <App />,
+    errorElement: (<div>Disco no Esta</div>)
+  },
+  {
+    path: "/info",
+    element: <Info />,
+    errorElement: (<div>Disco no Esta</div>)
+  },
+  {
+    path: "/Proximamente",
+    element: <Proximamente />,
+    errorElement: (<div>Disco no Esta</div>)
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <App />
+    <RouterProvider router={router} />
     <Footer />
   </StrictMode>,
 )
